8fbd4db87ade1fd7c85ee03fd556f3bd31e09573
lujainjumaa2@gmail.com <lujainjumaa2@gmail.com>
2025-06-14 14:59:35 +0300

    V jjj d 'bil'

--- workspaces/controller_ws/build/camera/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/r2w0t/dev/RCB/workspaces/controller_ws/src/camera
-# Build directory: /home/r2w0t/dev/RCB/workspaces/controller_ws/build/camera
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
