53582897833afc4dcee26b335d0cba9871578510
r2w0t <watertunner@gmail.com>
2025-08-15 17:25:18 +0300

    Added servo_angle topic and subscriber for it in rpi_ws/motor_control, and chaneged robot_interfaces/msg/Angle angle data type

--- workspaces/rpi_ws/src/motor_control/motor_control/motor_control_node.py
+++ workspaces/rpi_ws/src/motor_control/motor_control/motor_control_node.py
@@ -3,12 +3,14 @@ from rclpy.node import Node
 
 from std_msgs.msg import String
 
-from robot_interfaces.msg import Velocity
+from robot_interfaces.msg import Velocity, Angle
 
 import RPi.GPIO as GPIO
 import time
 
 import numpy as np
+
+servo_pin = 14
 
 left_motor_forward_pin = 4
 left_motor_reverse_pin = 17
@@ -26,20 +28,24 @@ A = np.linalg.inv(A)
 GPIO.setwarnings(False)		
 GPIO.setmode(GPIO.BCM)
 
+GPIO.setup(servo_pin,GPIO.OUT)
+servo_pwm = GPIO.PWM(servo_pin, 50)
+servo_pwm.start(0)
+
 GPIO.setup(right_motor_forward_pin,GPIO.OUT)
-right_forward_pwm = GPIO.PWM(right_motor_forward_pin, 1000)
+right_forward_pwm = GPIO.PWM(right_motor_forward_pin, frequency)
 right_forward_pwm.start(0)
 
 GPIO.setup(right_motor_reverse_pin,GPIO.OUT)
-right_reverse_pwm = GPIO.PWM(right_motor_reverse_pin, 1000)
+right_reverse_pwm = GPIO.PWM(right_motor_reverse_pin, frequency)
 right_reverse_pwm.start(0)
 
 GPIO.setup(left_motor_forward_pin,GPIO.OUT)
-left_forward_pwm = GPIO.PWM(left_motor_forward_pin, 1000)
+left_forward_pwm = GPIO.PWM(left_motor_forward_pin, frequency)
 left_forward_pwm.start(0)
 
 GPIO.setup(left_motor_reverse_pin,GPIO.OUT)
-left_reverse_pwm = GPIO.PWM(left_motor_reverse_pin, 1000)
+left_reverse_pwm = GPIO.PWM(left_motor_reverse_pin, frequency)
 left_reverse_pwm.start(0)
 
 class MotorControlNode(Node):
@@ -52,6 +58,24 @@ class MotorControlNode(Node):
             self.robot_velocity_callback,
             10)
         self.subscription  # prevent unused variable warning
+
+        self.servo_subscription = self.create_subscription(
+            Angle,
+            'servo_angle',
+            self.servo_angle_callback,
+            10)
+        self.servo_subscription  # prevent unused variable warning
+
+    # maps angle to pwm values
+    def angle_to_pwm_percent(self, angle):
+        pulse_width_ms = 1 + (angle / 180.0) * 1
+        pwm_percent = (pulse_width_ms / 20.0) * 100
+        return pwm_percent
+
+    def servo_angle_callback(self, msg):
+        pwm = self.angle_to_pwm_percent(msg.angle)
+        servo_pwm.ChangeDutyCycle(pwm)
+
 
     def robot_velocity_callback(self, msg):
         
